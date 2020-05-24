53bfcb9496846df13b7d78144d4ec510747c6268
Marcos Antonio Cordovil de Oliveira <marcoantonio.caco@gmail.com>
2020-05-24 13:56:34 -0300

    Commit inicial Janela basica com TKinter

---
+++ main.py
@@ -0,0 +1,19 @@
+import tkinter
+
+
+class BMTrack:
+    root_element = None
+
+    def __init__(self, root_element):
+        self.root_element = root_element
+
+        # Define o tamanho da janela principal para o tamanho da tela menos 200 pixels
+        self.root_element.geometry(f"{self.root_element.winfo_screenwidth() - 200}x{self.root_element.winfo_screenheight() - 200}")
+
+
+if __name__ == "__main__":
+    root = tkinter.Tk()
+
+    BMTrack(root)
+
+    root.mainloop()
