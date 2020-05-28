004cc7f11ddc109a1418fac13a9d5fc65e1f847a
Marcos Antonio Cordovil de Oliveira <marcoantonio.caco@gmail.com>
2020-05-28 12:42:05 -0300

    Abertura de arquivos de videos!

--- BiomecTrack/APP/BMTrack_gui.py
+++ BiomecTrack/APP/BMTrack_gui.py
@@ -9,34 +9,66 @@ class BMTrack:
     root_element = None
     logger = None
     camera_1_video = None
+    camera_1_frames = []
     camera_2_video = None
+    camera_2_frames = []
     camera_3_video = None
+    camera_3_frames = []
     camera_4_video = None
+    camera_4_frames = []
 
     def __init__(self, root_element):
+        print("Ola construido")
         self.root_element = root_element
         self.logger = Logger(logging)
 
         # Define o tamanho da janela principal para o tamanho da tela menos 200 pixels
         self.root_element.geometry(f"{self.root_element.winfo_screenwidth() - 200}x{self.root_element.winfo_screenheight() - 200}")
 
+        # Keyboard Binding
+        self.root_element.bind("<Key>", self.keyboard_handler)
+        self.root_element.bind("<Left>", self.keyboard_handler)
+        self.root_element.bind("<Right>", self.keyboard_handler)
+
         menubar = Menu(root_element)
 
         # create a pulldown menu, for loading the videos and add it to the menu bar
         video_menu = Menu(menubar, tearoff=0)
-        video_menu.add_command(label="Open Camera 1", command=lambda: self.load_video_file(1))
-        video_menu.add_command(label="Open Camera 2", command=lambda: self.load_video_file(2))
-        video_menu.add_command(label="Open Camera 3", command=lambda: self.load_video_file(3))
-        video_menu.add_command(label="Open Camera 4", command=lambda: self.load_video_file(4))
+        video_menu.add_command(label="Open Camera 1", command=lambda: self.load_video_file(0))
+        video_menu.add_command(label="Open Camera 2", command=lambda: self.load_video_file(1))
+        video_menu.add_command(label="Open Camera 3", command=lambda: self.load_video_file(2))
+        video_menu.add_command(label="Open Camera 4", command=lambda: self.load_video_file(3))
         menubar.add_cascade(label="Load Videos", menu=video_menu)
         self.root_element.configure(menu=menubar)
 
     def load_video_file(self, cam_number):
-        if cam_number == 1:
+        if cam_number == 0:
             self.camera_1_video = file_utils.open_video_file(title="Load Cam 1 Video FIle")
-        elif cam_number == 2:
+        elif cam_number == 1:
             self.camera_2_video = file_utils.open_video_file(title="Load Cam 2 Video FIle")
-        elif cam_number == 3:
+        elif cam_number == 2:
             self.camera_3_video = file_utils.open_video_file(title="Load Cam 3 Video FIle")
-        elif cam_number == 4:
+        elif cam_number == 3:
             self.camera_4_video = file_utils.open_video_file(title="Load Cam 4 Video FIle")
+    
+    def keyboard_handler(self, event):
+        print(f"pressed {repr(event.keysym)}")
+        print(event)
+
+        if event.keysym == 'o':
+            camera_counter = 0
+            while camera_counter < 4:
+                self.load_video_file(camera_counter)
+                camera_counter += 1
+        if event.keysym == '1':
+            self.camera_1_video = file_utils.open_video_file(title="Load Cam 1 Video FIle")
+        if event.keysym == '2':
+            self.camera_2_video = file_utils.open_video_file(title="Load Cam 2 Video FIle")
+        if event.keysym == '3':
+            self.camera_3_video = file_utils.open_video_file(title="Load Cam 3 Video FIle")
+        if event.keysym == '4':
+            self.camera_4_video = file_utils.open_video_file(title="Load Cam 4 Video FIle")
+        if event.keysym == "Left":
+            print("arrow l")
+        if event.keysym == "Right":
+            print("arrow r")
